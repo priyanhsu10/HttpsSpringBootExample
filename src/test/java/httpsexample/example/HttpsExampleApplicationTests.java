1b5119c75e7d16474d9322d2bb562a7884930d4b
priyanhsu10 <priyanshuparate@yahoo.in>
2021-09-06 15:20:50 +0530

    Initial

---
+++ src/test/java/httpsexample/example/HttpsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package httpsexample.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
